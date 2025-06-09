a6ef87caff5fc21eeb516350279aac6a3ec00986
Lucky <lucky@Luckys-MBP.fritz.box>
2025-06-09 19:28:54 +0200

    Update footer design and add web friends linking

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,23 +2,47 @@ import { IconBrandX, IconBrandInstagram } from "@tabler/icons-react";
 import Link from "next/link";
 import styles from "./Footer.module.scss";
 
-const { mainWrapper, containerStyles, socialIcons } = styles;
+const {
+  mainWrapper,
+  containerStyles,
+  socialIcons,
+  containerWrapperStyles,
+  footerTitle,
+  linkItemStyles,
+  linkingSectionStyles,
+  copyrightStyles,
+} = styles;
 
 export const Footer = () => {
   return (
     <footer className={mainWrapper}>
-      <div className={containerStyles}>
-        <div className={socialIcons}>
-          <ul>
-            <li>
-              <Link href="https://www.instagram.com/buenoshumoszaragoza">
-                <IconBrandInstagram size={48} />
-              </Link>
-            </li>
-          </ul>
+      <div className={containerWrapperStyles}>
+        <p className={footerTitle}>Buenos Humos Zaragoza</p>
+        <div className={containerStyles}>
+          <div className={linkingSectionStyles}>
+            <h4>Nuestros Amigos</h4>
+            <ul>
+              <li className={linkItemStyles}>
+                <Link href="https://www.cigarsmokerclub.com">
+                  Cigar Smoker Club
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={socialIcons}>
+            <h4>Síguenos en nuestras redes sociales</h4>
+            <ul>
+              <li>
+                <Link href="https://www.instagram.com/buenoshumoszaragoza">
+                  <IconBrandInstagram size={48} />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <p>Copyright ©2024 Todos los derechos reservados</p>
-        {/* <p>Términos y condiciones / Política de privacidad</p> */}
+        <p className={copyrightStyles}>
+          Copyright ©2024 Todos los derechos reservados
+        </p>
       </div>
     </footer>
   );
